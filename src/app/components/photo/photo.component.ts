8d12e918a0169bc29588e7a250b6f730a062e881
Nikk-V-V <nikola716@gmail.com>
2021-05-07 20:09:26 +0300

    5 files modified, 2 added

--- src/app/components/photo/photo.component.ts
+++ src/app/components/photo/photo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-photo',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo.component.scss']
 })
 export class PhotoComponent implements OnInit {
+  @Input('photos') photos: Photo[];
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo, Post } from '../../shared/interfaces';
+import { PhotoService } from '../../shared/services/photo.service';
+import { PostService } from '../../shared/services/post.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  posts: Post[];
+  photos: Photo[];
 
-  ngOnInit() {}
+  constructor(
+    private photoService: PhotoService,
+    private postService: PostService
+  ) {}
+
+  ngOnInit() {
+    this.get();
+  }
+
+  get() {
+    this.getTopPosts();
+    this.getTopPhotos();
+  }
+
+  getTopPosts() {
+    this.postService.getAll().subscribe((posts: Post[]) => {
+      this.posts = posts.slice(0, 9);
+      console.log(this.posts);
+    });
+  }
+
+  getTopPhotos() {
+    this.photoService.getAll().subscribe((photos: Photo[]) => {
+      this.photos = photos.slice(0, 9);
+      console.log(this.photos);
+    });
+  }
 }
