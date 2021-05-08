9de025c455dd52d2baefbf67be4dca2939b9def6
Nikk-V-V <nikola716@gmail.com>
2021-05-08 16:48:31 +0300

    3 files modified

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -13,11 +13,14 @@ export class ContactUsComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       fullname: new FormControl(null, [Validators.required]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      email: new FormControl(null, [
+        Validators.required, 
+        Validators.email,
+        Validators.pattern(/^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i)
+        ]),
       phoneNumber: new FormControl('+380', [
         Validators.required,
         Validators.minLength(13),
-        Validators.maxLength(13),
         Validators.pattern(
           /^((\+?3)?8)?((0\(\d{2}\)?)|(\(0\d{2}\))|(0\d{2}))\d{7}$/
         )
